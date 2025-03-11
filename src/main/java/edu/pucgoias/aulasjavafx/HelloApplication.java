e4d8d3f02eb45570f06dc8ed625dc6b0362b2d2a
Thalles Santos <thallesbgns@gmail.com>
2025-03-11 17:25:08 -0300

    Finalizando alterações dop Form 01

--- src/main/java/edu/pucgoias/aulasjavafx/HelloApplication.java
+++ src/main/java/edu/pucgoias/aulasjavafx/HelloApplication.java
@@ -1,14 +1,11 @@
 package edu.pucgoias.aulasjavafx;
 
 import javafx.application.Application;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.StackPane;
-import javafx.scene.text.Text;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class HelloApplication extends Application {
@@ -18,18 +15,32 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        StackPane pane = new StackPane();
+        AnchorPane pane = new AnchorPane();
         pane.setPrefSize(400, 300);
 
         TextField txtName = new TextField();
         txtName.setPromptText("Adicione o nome");
-        TextField txtPass = new TextField();
-        txtPass.setPromptText("Adicione a senha");
+        pane.getChildren().add(txtName);
 
-        pane.getChildren().addAll(txtName, txtPass);
+        PasswordField txtPass = new PasswordField();
+        txtPass.setPromptText("Adicione a senha");
+        pane.getChildren().add(txtPass);
+
+        Button btnEnter = new Button("Entrar");
+        pane.getChildren().add(btnEnter);
 
         Scene scene = new Scene(pane, 400, 300);
+        stage.setTitle("Formulário 01");
         stage.setScene(scene);
         stage.show();
+
+        txtName.setLayoutX((pane.getWidth() - txtName.getWidth()) / 2);
+        txtName.setLayoutY(50);
+
+        txtPass.setLayoutX((pane.getWidth() - txtPass.getWidth()) / 2);
+        txtPass.setLayoutY(100);
+
+        btnEnter.setLayoutX((pane.getWidth() - btnEnter.getWidth()) / 2);
+        btnEnter.setLayoutY(150);
     }
 }
