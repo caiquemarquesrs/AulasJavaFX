ce7b5ad9464c6ac828147f1189e2d4fdfbbc6993
Thalles Santos <thallesbgns@gmail.com>
2025-03-11 11:43:30 -0300

    Trabalhando com imagens

--- src/main/java/edu/pucgoias/aulasjavafx/HelloApplication.java
+++ src/main/java/edu/pucgoias/aulasjavafx/HelloApplication.java
@@ -1,37 +1,42 @@
 package edu.pucgoias.aulasjavafx;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-import java.util.Stack;
-
 public class HelloApplication extends Application {
-    @Override
-    public void start(Stage palco) throws IOException {
-        /*FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();*/
 
-        StackPane raiz = new StackPane();
-        Label lblMensagem = new Label();
+    /*private final String IMG_URL =
+            "https://www.itabirito.mg.leg.br/imagens/face.png/image";*/
 
-        lblMensagem.setText("Iniciando com JavaFX!");
-        raiz.getChildren().add(lblMensagem);
-
-        Scene cena = new Scene(raiz, 250, 100);
-        palco.setTitle("Learning JavaFX");
-        palco.setScene(cena);
-        palco.show();
-    }
+    private final String IMG_URL =
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpevfm2P0BtgC9LbXH1n2Xp-oR-iLX8xQTBg&s";
 
     public static void main(String[] args) {
         launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Image image = new Image(IMG_URL);
+        ImageView viewImage = new ImageView(image);
+        viewImage.setTranslateX(0);
+        viewImage.setTranslateY(0);
+
+        Text txtInfo = new Text("Logo X desenhado com JavaFX");
+        txtInfo.setTranslateX(10);
+        txtInfo.setTranslateY(15);
+
+        Group components = new Group(); // 9
+        components.getChildren().addAll(viewImage,
+                txtInfo);
+        Scene cena = new Scene(components, 250, 250);
+        stage.setTitle("Imagens em JavaFX");
+        stage.setScene(cena);
+        stage.show();
     }
 }
